f5a723f3178105f2d5d1fa01a2888564cbb9df0e
MRXY001 <wxy19980615@gmail.com>
2020-04-30 17:50:53 +0800

    更新API链接

--- netease/neteaseapi.h
+++ netease/neteaseapi.h
@@ -3,6 +3,8 @@
 
 #include <QString>
 
+#define NETEASE_DOMAIN "49.235.46.194:3000/netease/"
+
 class NeteaseAPI
 {
 public:
@@ -11,7 +13,8 @@ public:
      */
     static inline QString getSongListSearchUrl(QString type)
     {
-        return "https://v1.itooi.cn/netease/search?keyword="+type+"&type=songList";
+//        return "https://v1.itooi.cn/netease/search?keyword="+type+"&type=songList";
+        return NETEASE_DOMAIN + "search?type=1000&keywords=" + type;
     }
 
     /**
@@ -19,7 +22,8 @@ public:
      */
     static inline QString getSongListUrl(QString id)
     {
-        return "https://v1.itooi.cn/netease/songList?id=" + id;
+//        return "https://v1.itooi.cn/netease/songList?id=" + id;
+        return NETEASE_DOMAIN + "playlist/detail?id=" + id;
     }
 
     /**
@@ -28,16 +32,18 @@ public:
      */
     static inline QString getSongUrl(QString id)
     {
-        return "https://v1.itooi.cn/netease/song?id="+id;
+//        return "https://v1.itooi.cn/netease/song?id="+id;
+        return NETEASE_DOMAIN + "song/detail?ids=" + id;
     }
 
     /**
      * 获取歌曲下载地址
+     * NodeJS版地址在 SongUrl 获取到的结果中有
      */
     static inline QString getSongDownloadUrl(QString id)
     {
-//        return "https://v1.itooi.cn/netease/url?id="+id+"&quality=flac";
-        return "https://v1.itooi.cn/netease/url?id="+id;
+//        return "https://v1.itooi.cn/netease/url?id="+id;
+        return NETEASE_DOMAIN + "song/url?id=" + id;
     }
 };
 
